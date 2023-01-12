72001b65fc1e4578ff8c093f07ecd0f5eedcc388
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-01-12 11:44:36 -0800

    process in batches to not totally destroy system performance

--- make-bulk.sh
+++ make-bulk.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for i in {00001..00050}
-do
-	ASSETID="E$i" make default &
+#Parallel runs in N-process batches
+# https://unix.stackexchange.com/a/216475/
+
+#batch size
+N=5
+(
+for id in {00001..00050}; do 
+   ((i=i%N)); ((i++==0)) && wait
+   	ASSETID="E$id" make default &
 done
+wait
+echo "Done."
+exit 0
+)
