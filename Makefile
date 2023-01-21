618aa272dc173de9b96dfdc503acfafa714ae5a3
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-01-20 20:03:21 -0500

    the rest of the f*cking owl (big labels)

--- Makefile
+++ Makefile
@@ -1,21 +1,25 @@
-.PHONY: default
 # ASSETID=
 BARCODERAWTMP = "barcode$(ASSETID).eps"
 BARCODETMP = "barcode$(ASSETID).png"
 QRTMP = "qrcode$(ASSETID).png"
 OUTPUT = "label-$(ASSETID).png"
+PROPERTY_LABEL = "Property of RIT Rowing"
+WIDTH = 875
+HEIGHT = 625
 # this is a bit of a hack to generate barcodes closer in size to the label itself to prevent issues when scaling the barcode. its value is the width of the label that the python script generates minus two times the padding value (to give it some space on the left and right)
-BARCODE_WIDTH = 865
+BARCODE_WIDTH = 845
+BARCODE_HEIGHT = 100
+
 barcode:
-	barcode -o $(BARCODERAWTMP) -b $(ASSETID) -n -e "128" -g "$(BARCODE_WIDTH)x50" -E
+	barcode -o $(BARCODERAWTMP) -b $(ASSETID) -n -e "128" -g "$(BARCODE_WIDTH)x$(BARCODE_HEIGHT)" -E
 	convert $(BARCODERAWTMP) -colorspace RGB -background white -flatten $(BARCODETMP)
 	rm $(BARCODERAWTMP)
 
-qr: 
+qr:
 	qrencode -o $(QRTMP) $(ASSETID)
 
 default: qr barcode
-	pipenv run python3 ./labelgen.py --qr-path $(QRTMP) --barcode-path $(BARCODETMP) --label $(ASSETID) --output $(OUTPUT)
+	pipenv run python3 ./labelgen.py --qr-path $(QRTMP) --barcode-path $(BARCODETMP) --label $(ASSETID) --width "$(WIDTH)" --height "$(HEIGHT)" --property $(PROPERTY_LABEL) --output $(OUTPUT)
 	rm $(BARCODETMP) $(QRTMP)
 
 
--- labelgen.py
+++ labelgen.py
@@ -35,11 +35,11 @@ args = parser.parse_args()
 
 background = Image.new('RGBA', (args.width, args.height), (255, 255, 255, 255))
 bg_w, bg_h = background.size
-padding = 5
+padding = int(.025*args.width)
 
 if args.qr_path:
 	qr = Image.open(args.qr_path, 'r')
-	qr_height_factor = .8
+	qr_height_factor = .55
 	qr_size = int(qr_height_factor*bg_h)
 	qr = qr.resize((qr_size,qr_size))
 	qr_w, qr_h = qr.size
@@ -75,19 +75,58 @@ if args.barcode_path:
 
 
 if args.label:
-	draw = ImageDraw.Draw(background)
-	# font = ImageFont.truetype(<font-file>, <font-size>)
-	font = ImageFont.truetype("/usr/share/fonts/truetype/open-sans/OpenSans-Bold.ttf", 128)
-	font_w, font_h = draw.textsize(args.label,font=font)
 
-	w=(bg_w-font_w)/2
-	if args.qr_path:
-		w += qr_w/2
+	text = args.label
+	# apply special processing for alpha-prefixed numeric asset tags to split it up
+	print(splitAlphaAndNumeric(text))
+	lines = splitAlphaAndNumeric(text)
+
+	draw = ImageDraw.Draw(background)
+	alphafont = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf", 64)
+	numfont = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf", 220)
+
+
+	start_w = 0
+	start_h = 0
+
 	
-	h=(bg_h-font_h)/2
-	if args.barcode_path:
-		h -= bcode_h/2
-	draw.text((w,h), args.label, fill="black",font=font)
+	# num_w, num_h = draw.textsize(lines[0],font=numfont)
+
+
+	if args.qr_path:
+		start_w = qr_w + qr_x - 20
+		start_h = qr_y + 35#+ int(padding/2)#+ qr_y
+		#int((bg_h-bcode_h-padding-alpha_h-num_h)/2)
+	# draw text
+	alpha_w, alpha_h = draw.textsize(lines[0],font=alphafont)
+	
+	print(alpha_h)
+	draw.text((start_w, start_h), lines[0], fill="black",font=alphafont)
+
+	# draw number
+	draw.text((start_w, start_h + alpha_h), lines[1], fill="black",font=numfont)
+
+	# for line in lines:
+	# 	width, height = draw.textsize(line,font=font)
+	# 	draw.text(((w - width) / 2, y_text), line, fill="black",font=font)
+	# 	y_text += height
+	
+
+	# font = ImageFont.truetype(<font-file>, <font-size>)
+	
+	# font_w, font_h = draw.textsize(lines[1],font=numfont)
+
+	# w=(bg_w-font_w)/2
+	# if args.qr_path:
+	# 	w += qr_w/2 - 25
+	
+	# h=(bg_h-font_h)/2
+	# if args.barcode_path:
+	# 	h -= bcode_h/2
+	# start_w, start_h = (w,h)
+	
+	# draw.text((w,h), lines[1], fill="black",font=numfont)
+
 if args.property:
 	draw = ImageDraw.Draw(background)
 
