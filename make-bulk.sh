de0096c42e007a31b5ff30e8dc2d6fcbbe121bf5
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-01-22 16:15:34 -0500

    turn the bulk label generation into a function

--- make-bulk.sh
+++ make-bulk.sh
@@ -3,14 +3,29 @@
 #Parallel runs in N-process batches
 # https://unix.stackexchange.com/a/216475/
 
-#batch size
-N=5
-(
-for id in {00001..00050}; do 
-   ((i=i%N)); ((i++==0)) && wait
-   	ASSETID="E$id" make default &
-done
-wait
-echo "Done."
-exit 0
-)
+OUTPUTDIR="output/"
+PREFIX="E"
+
+function make_batch {
+	# $1 - startng id
+	# $2 - ending id
+	# $3 - style (default: "default")
+	# $4 -batch size (default: 5)
+	STYLE="${3:-default}"
+	echo $1
+	echo $2
+	#batch size
+	N="${4:-5}"
+	(
+	for id in $(seq -w $1 $2); do 
+	((i=i%N)); ((i++==0)) && wait
+		ASSETID="$PREFIX$id" OUTPUTDIR="$OUTPUTDIR" make $STYLE &
+	done
+	wait
+	echo "Done."
+	exit 0
+	)
+
+}
+
+mkdir -p "$OUTPUTDIR"
