aa7c55cf1cf3fd21096923bf40a2056a6b6d38dd
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-01-22 14:59:59 -0500

    add configFinder class

---
+++ configfinder.py
@@ -0,0 +1,28 @@
+from configparser import ConfigParser, NoOptionError
+
+class ConfigFinder:
+
+	def __init__(self, configfilename, configsection="Default"):
+		self.config = ConfigParser()
+		self.config.read(configfilename)
+		self.section = configsection
+
+	def getString(self, key, default=""):
+		try:
+			return self.config.get(self.section, key)
+		except (KeyError, NoOptionError) as e:
+			return default
+
+	def getInteger(self, key):
+		val = self.getString(key, default=None)
+		if val:
+			return int(val)
+		else:
+			return val
+
+	def getFloat(self, key):
+		val = self.getString(key, default=None)
+		if val:
+			return float(val)
+		else:
+			return val
