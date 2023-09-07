e1e66f64867d920e5ed2e4c0caaf10f3842876ac
Adrian Edwards <adrian@adriancedwards.com>
2023-09-07 13:50:41 -0400

    add bulk script functions for making a single HRM label and batching them

--- make-bulk.sh
+++ make-bulk.sh
@@ -28,6 +28,38 @@ function make_batch {
 
 }
 
+function make_hrm {
+	# $1 - id
+	# $2 - style
+	# $3 - property label
+
+	pipenv run python3 ./labelgen.py --label "$PREFIX$1" --barcode --propertylabel "$3" --configsection "$2" --output "$OUTPUTDIR$PREFIX$1.png"
+}
+
+
+function make_batch_hrm {
+	# $1 - startng id
+	# $2 - ending id
+	# $3 - style (default: "default")
+	# $4 -batch size (default: 5)
+	# $5 - property label
+	STYLE="${3:-default}"
+	echo $1
+	echo $2
+	#batch size
+	N="${4:-5}"
+	(
+	for id in $(seq -w $1 $2); do 
+	((i=i%N)); ((i++==0)) && wait
+		make_hrm "$id" "$STYLE" "BT# $id"  &
+	done
+	wait
+	echo "Done."
+	exit 0
+	)
+
+}
+
 mkdir -p "$OUTPUTDIR"
 
 PREFIX="DEMO"
